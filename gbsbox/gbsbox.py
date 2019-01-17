abf21f6a1f56f6766361d231346d862aa4baf2b7
rusydi <rusydi.hasan@gmail.com>
2019-01-17 14:21:44 +0100

    major modification

--- gbsbox/gbsbox.py
+++ gbsbox/gbsbox.py
@@ -15,10 +15,10 @@ from six import integer_types
 from sage.rings.integer import Integer
 from sage.crypto.sbox import SBox
 from sage.matrix.constructor import Matrix
+from sage.modules.free_module_element import vector
 from sage.rings.finite_rings.finite_field_constructor import GF
 from sage.rings.ideal import Ideal, FieldIdeal
 from sage.rings.integer_ring import ZZ
-from sage.rings.polynomial.pbori import BooleanPolynomialRing
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
 from sage.structure.sequence import Sequence
 
@@ -35,48 +35,64 @@ class GBSBox(SBox):
 
         - ``big_endian`` - controls whether bits shall be ordered in
           big endian order (default: ``True``)
+
+        - ``indexing`` - indexing of variables in the ring (default : 0)
         """
         super(GBSBox, self).__init__(*args, **kwargs)
-        self._algdiff_ring = None
 
-    def algebraic_differential_ring(self, order="degrevlex", use_polybori=False):
+        self._linear_ring = None
+        self._differential_ring = None
+        self._indexing = kwargs.get("indexing", 0)
+
+
+    def linear_ring(self, order="degrevlex"):
         """
-        Return a polynomial ring for algebraic-differential analysis.
+        Return a polynomial ring for linear cryptanalysis.
 
         INPUT:
 
         - ``order`` - a monomial order (default : degrevlex)
-        - ``use_polybori`` - use Boolean polynomial ring (default : False)
 
         EXAMPLES::
 
             sage: from gbsbox import GBSBox
-            sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2)
-            sage: GS.algebraic_differential_ring()
-            Multivariate Polynomial Ring in xl0, xl1, xl2, yl0, yl1, yl2, xr0, xr1, xr2, yr0, yr1, yr2 over Finite Field of size 2
-            sage: GS.algebraic_differential_ring(order="deglex", use_polybori=True)
-            Boolean PolynomialRing in xl0, xl1, xl2, yl0, yl1, yl2, xr0, xr1, xr2, yr0, yr1, yr2
+            sage: GS = GBSBox([0, 1, 3, 6, 7, 4, 5, 2], big_endian=False, indexing=1)
+            sage: GS.linear_ring()
+            Multivariate Polynomial Ring in x1, x2, x3, y1, y2, y3 over Finite Field of size 2
         """
+        if self._linear_ring is not None and self._linear_ring.term_order().name() == order:
+            return self._linear_ring
 
-        if self._algdiff_ring is not None and use_polybori is False:
-            return self._algdiff_ring
+        var_names = self.varstrs("x") + self.varstrs("y")
+        self._linear_ring = PolynomialRing(GF(2), var_names, order=order)
+
+        return self._linear_ring
+
+
+    def differential_ring(self, order="degrevlex"):
+        """
+        Return a polynomial ring for differential cryptanalysis.
+
+        INPUT:
+
+        - ``order`` - a monomial order (default : degrevlex)
+
+        EXAMPLES::
+
+            sage: from gbsbox import GBSBox
+            sage: GS = GBSBox([0, 1, 3, 6, 7, 4, 5, 2], indexing=1)
+            sage: GS.differential_ring()
+            Multivariate Polynomial Ring in xl1, xl2, xl3, yl1, yl2, yl3, xr1, xr2, xr3, yr1, yr2, yr3 over Finite Field of size 2
+        """
+        if self._differential_ring is not None and self._differential_ring.term_order().name() == order:
+            return self._differential_ring
 
         varxl, varyl = self.varstrs("x", "l"), self.varstrs("y", "l")
         varxr, varyr = self.varstrs("x", "r"), self.varstrs("y", "r")
         var_names = varxl + varyl + varxr + varyr
+        self._differential_ring = PolynomialRing(GF(2), var_names, order=order)
 
-        if use_polybori:
-            # The Boolean polynomial ring is needed to convert the polynomials into
-            # Boolean polynomials for faster Grobner bases computation.
-            R = BooleanPolynomialRing(len(var_names), var_names, order=order)
-        else:
-            # Computing algebraic representation of an SBox must be done using
-            # polynomial ring of Singular because calling self.polynomials(X, Y) is slow
-            # when X, Y are defined over Boolean polynomial ring
-            R = PolynomialRing(GF(2), var_names, order=order)
-            self._algdiff_ring = R
-
-        return R
+        return self._differential_ring
 
 
     def varformatstr(self, name, postfix):
@@ -85,8 +101,8 @@ class GBSBox(SBox):
 
         INPUT:
 
-        - ``name`` - name of variable
-        - ``postfix`` - postfix string
+        - ``name`` -- name of variable
+        - ``postfix`` -- postfix string
 
         EXAMPLES::
 
@@ -98,14 +114,14 @@ class GBSBox(SBox):
         return name + postfix + "%d"
 
 
-    def varstrs(self, name, postfix):
+    def varstrs(self, name, postfix=""):
         """
         Return a tuple of variables named `name` with postfix `postfix`.
 
         INPUT:
 
-        - ``name`` - name of variable
-        - ``postfix`` - postfix string
+        - ``name`` -- name of variable
+        - ``postfix`` -- postfix string
 
         EXAMPLES::
 
@@ -118,39 +134,78 @@ class GBSBox(SBox):
             size = self.input_size()
         elif name == 'y':
             size = self.output_size()
+        else:
+            raise ValueError("name of variables must either be x or y")
 
-        return tuple(self.varformatstr(name, postfix) % (i) for i in range(size))
+        idx = self._indexing
+        return tuple(self.varformatstr(name, postfix) % (i) for i in range(idx, size+idx))
 
 
-    def vars(self, name):
+    def vars(self, name, R):
         """
-        Return a tuple of variables for variables named `name`.
+        Return a list of variables for variables named `name` in the ring `R`.
 
         INPUT:
 
-        - ``name`` -- name of variable
-
-        EXAMPLES::
-
-            sage: from gbsbox import GBSBox
-            sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2, big_endian=False)
-            sage: GS.vars('xl')
-            [xl0, xl1, xl2]
-            sage: GS.vars('y')
-            [yl0, yl1, yl2, yr0, yr1, yr2]
-            sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2, big_endian=True)
-            sage: GS.vars('xl')
-            [xl2, xl1, xl0]
-            sage: GS.vars('y')
-            [yr2, yr1, yr0, yl2, yl1, yl0]
+        - ``name`` - name of variable
+        - ``R`` - polynomial ring
         """
-        R = self.algebraic_differential_ring()
-        v = [R(s) for s in R.variable_names() if s.startswith(name)]
+        v = [ R(s) for s in R.variable_names() if s.startswith(name) ]
 
         if self._big_endian is True:
             v.reverse()
 
         return v
+
+
+    def linear_vars(self, name):
+        """
+        Return a list of variables for variables named `name` for linear cryptanalysis.
+
+        INPUT:
+
+        - ``name`` - name of variables
+
+        EXAMPLES::
+
+            sage: from gbsbox import GBSBox
+            sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2, big_endian=False)
+            sage: GS.linear_vars('x')
+            [x0, x1, x2]
+            sage: GS.linear_vars('y')
+            [y0, y1, y2]
+            sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2, big_endian=True)
+            sage: GS.linear_vars('x')
+            [x2, x1, x0]
+            sage: GS.linear_vars('y')
+            [y2, y1, y0]
+        """
+        return self.vars(name, self.linear_ring())
+
+
+    def differential_vars(self, name):
+        """
+        Return a list of variables for variables named `name` for differential cryptanalysis.
+
+        INPUT:
+
+        - ``name`` - name of variables
+
+        EXAMPLES::
+
+            sage: from gbsbox import GBSBox
+            sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2, big_endian=False)
+            sage: GS.differential_vars('xl')
+            [xl0, xl1, xl2]
+            sage: GS.differential_vars('y')
+            [yl0, yl1, yl2, yr0, yr1, yr2]
+            sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2, big_endian=True)
+            sage: GS.differential_vars('xl')
+            [xl2, xl1, xl0]
+            sage: GS.differential_vars('y')
+            [yr2, yr1, yr0, yl2, yl1, yl0]
+        """
+        return self.vars(name, self.differential_ring())
 
 
     def twoinput_polynomials(self):
@@ -193,8 +248,8 @@ class GBSBox(SBox):
             yr0*yr1 + xr2 + yr1 + yr2
         """
 
-        xl, yl = self.vars('xl'), self.vars('yl')
-        xr, yr = self.vars('xr'), self.vars('yr')
+        xl, yl = self.differential_vars('xl'), self.differential_vars('yl')
+        xr, yr = self.differential_vars('xr'), self.differential_vars('yr')
 
         F = []
         Fl, Fr = [], []
@@ -208,21 +263,98 @@ class GBSBox(SBox):
         return Sequence(F)
 
 
-    def algebraic_differential_polynomials(self, a, b=None, order="deglex"):
+    def linear_ideal(self, a, b, order="degrevlex"):
         """
-        Return a system of equations for algebraic-differential analysis.
+        Return the linear ideal with input mask `a` and output mask `b`.
 
         INPUT:
 
-        - ``a`` -- input difference
-        - ``b`` -- output difference (optional)
-        - ``order`` -- monomial ordering (default : deglex)
+        - ``a`` -- input mask
+        - ``b`` -- output mask
+        - ``order`` - monomial ordering (default : degrevlex)
 
         EXAMPLES::
 
             sage: from gbsbox import GBSBox
             sage: GS = GBSBox(0, 3, 5, 8, 6, 10, 15, 4, 14, 13, 9, 2, 1, 7, 12, 11)
-            sage: F = GS.algebraic_differential_polynomials(4, order="lex")
+            sage: I = GS.linear_ideal(3, 1)
+            sage: (I.vector_space_dimension() - 8) == 4
+            True
+        """
+        m, n = self.input_size(), self.output_size()
+        x, y = self.linear_vars('x'), self.linear_vars('y')
+
+        if isinstance(a, integer_types + (Integer,)):
+            a = self.to_bits(a, m)
+        if isinstance(b, integer_types + (Integer,)):
+            b = self.to_bits(b, n)
+
+        F = []
+        deg = 2
+        while not F:
+            F = self.polynomials(X=x, Y=y, degree=deg)
+            deg += 1
+
+        F += [ sum(a[i]*x[i] for i in range(m)) + sum(b[j]*y[j] for j in range(n)) ]
+
+        R = self.linear_ring(order)
+        I = Ideal(R, F) + FieldIdeal(R)
+
+        return I
+
+
+    def autocorrelation_ideal(self, a, b, order="degrevlex"):
+        """
+        Return the autocorrelation ideal with input difference `a` and output mask `b`.
+
+        INPUT:
+
+        - ``a`` - input difference
+        - ``b`` - output mask
+        - ``order`` - monomial ordering (default : degrevlex)
+
+        EXAMPLES::
+
+            sage: from gbsbox import GBSBox
+            sage: GS = GBSBox(0, 3, 5, 8, 6, 10, 15, 4, 14, 13, 9, 2, 1, 7, 12, 11)
+            sage: I = GS.autocorrelation_ideal(3, 1)
+            sage: act = GS.autocorrelation_table()
+            sage: (16 - 2*I.vector_space_dimension()) == act[3, 1]
+            True
+        """
+        F = self.twoinput_polynomials()
+
+        xl, xr = self.differential_vars("xl"), self.differential_vars("xr")
+        yl, yr = self.differential_vars("yl"), self.differential_vars("yr")
+        m, n = self.input_size(), self.output_size()
+        if isinstance(a, integer_types + (Integer,)):
+            a = self.to_bits(a, m)
+        if isinstance(b, integer_types + (Integer,)):
+            b = self.to_bits(b, n)
+        F += [ xl[i] + xr[i] + a[i] for i in range(m) ]
+        F += [ sum([ b[j]*(yl[j] + yr[j]) for j in range(n) ]) + 1 ]
+
+        R = self.differential_ring(order)
+        I = Ideal(R, F) + FieldIdeal(R)
+
+        return I
+
+
+    def differential_ideal(self, a, b=None, order="degrevlex"):
+        """
+        Return a multivariate ideal for differential cryptanalysis.
+
+        INPUT:
+
+        - ``a`` -- input difference
+        - ``b`` -- output difference (optional)
+        - ``order`` -- monomial ordering (default : degrevlex)
+
+        EXAMPLES::
+
+            sage: from gbsbox import GBSBox
+            sage: GS = GBSBox(0, 3, 5, 8, 6, 10, 15, 4, 14, 13, 9, 2, 1, 7, 12, 11)
+            sage: F = GS.differential_ideal(4, order="lex")
             sage: G = F.groebner_basis()
             sage: for g in G: print(g)
             xl0 + yr0*yr1*yr3 + yr0*yr2*yr3 + yr0*yr2 + yr0*yr3 + yr1*yr3 + yr1 + yr2 + yr3
@@ -237,26 +369,30 @@ class GBSBox(SBox):
             xr1 + yr0*yr1*yr2 + yr0*yr1 + yr0*yr2*yr3 + yr1 + yr2*yr3 + yr2 + yr3
             xr2 + yr0*yr1*yr3 + yr0*yr1 + yr0*yr2*yr3 + yr0*yr3 + yr0 + yr1*yr3 + yr2
             xr3 + yr0*yr1*yr2 + yr0*yr2 + yr0 + yr1*yr2*yr3 + yr1*yr2 + yr1*yr3 + yr1 + yr2*yr3
+            yr0^2 + yr0
+            yr1^2 + yr1
+            yr2^2 + yr2
+            yr3^2 + yr3
         """
-
         F = self.twoinput_polynomials()
 
-        xl, xr = self.vars("xl"), self.vars("xr")
+        xl, xr = self.differential_vars("xl"), self.differential_vars("xr")
         m = self.input_size()
         if isinstance(a, integer_types + (Integer,)):
             a = self.to_bits(a, m)
         F += [ xl[i] + xr[i] + a[i] for i in range(m) ]
 
         if b is not None:
-            yl, yr = self.vars("yl"), self.vars("yr")
+            yl, yr = self.differential_vars("yl"), self.differential_vars("yr")
             n = self.output_size()
             if isinstance(b, integer_types + (Integer,)):
                 b = self.to_bits(b, n)
             F += [ yl[i] + yr[i] + b[i] for i in range(n) ]
 
-        B = self.algebraic_differential_ring(order=order, use_polybori=True)
+        R = self.differential_ring(order=order)
+        I = Ideal(R, F) + FieldIdeal(R)
 
-        return Sequence(F, B)
+        return I
 
 
     def gb_linear_structures(self):
@@ -277,11 +413,11 @@ class GBSBox(SBox):
         """
         ret = []
         F = self.twoinput_polynomials()
-        B = self.algebraic_differential_ring(order="deglex", use_polybori=True)
+        B = self.differential_ring()
 
         m, n = self.input_size(), self.output_size()
-        xl, yl = self.vars("xl"), self.vars("yl")
-        xr, yr = self.vars("xr"), self.vars("yr")
+        xl, yl = self.differential_vars("xl"), self.differential_vars("yl")
+        xr, yr = self.differential_vars("xr"), self.differential_vars("yr")
 
         for a in range(1, 1 << m):
             va = self.to_bits(a, m)
@@ -298,6 +434,48 @@ class GBSBox(SBox):
         return ret
 
 
+    def gb_linear_bias(self, a, b):
+        """
+        Return the linear bias of this S-Box.
+
+        INPUT:
+
+        - ``a`` - input mask
+        - ``b`` - output mask
+
+        EXAMPLES::
+
+            sage: from gbsbox import GBSBox
+            sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2)
+            sage: lat = GS.linear_approximation_table()
+            sage: lat[1, 3] == GS.gb_linear_bias(1, 3)
+            True
+        """
+        I = self.linear_ideal(a, b)
+        return I.vector_space_dimension() - (1 << (self.input_size() - 1))
+
+
+    def gb_autocorrelation(self, a, b):
+        """
+        Return the autocorrelation of this S-Box.
+
+        INPUT:
+
+        - ``a`` - input difference
+        - ``b`` - output mask
+
+        EXAMPLES::
+
+            sage: from gbsbox import GBSBox
+            sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2)
+            sage: act = GS.autocorrelation_table()
+            sage: act[1, 3] == GS.gb_autocorrelation(1, 3)
+            True
+        """
+        I = self.autocorrelation_ideal(a, b)
+        return (1 << self.input_size()) - (2 * I.vector_space_dimension())
+
+
     def gb_differential_probability(self, a, b, get_numerator=False):
         """
         Return the differential probability of this S-Box.
@@ -312,15 +490,11 @@ class GBSBox(SBox):
 
             sage: from gbsbox import GBSBox
             sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2)
-            sage: ddt = GS.difference_distribution_matrix()
+            sage: ddt = GS.difference_distribution_table()
             sage: ddt[1, 3] == GS.gb_differential_probability(1, 3, get_numerator=True)
             True
         """
-
-        F = self.algebraic_differential_polynomials(a, b)
-        G = F.groebner_basis()
-        R = self.algebraic_differential_ring(order=G[0].parent().term_order(), use_polybori=False)
-        I = Ideal(R, G) + FieldIdeal(R)
+        I = self.differential_ideal(a, b)
         num = I.vector_space_dimension()
 
         ret = 0
@@ -332,16 +506,56 @@ class GBSBox(SBox):
         return ret
 
 
-    def gb_difference_distribution_matrix(self):
+    def gb_linear_approximation_table(self):
         """
-        Return a difference distribution matrix using Grobner bases algorithm.
+        Return the linear approximation table using Grobner bases algorithm.
 
         EXAMPLES::
 
             sage: from gbsbox import GBSBox
             sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2)
-            sage: ddt = GS.difference_distribution_matrix()
-            sage: gb_ddt = GS.gb_difference_distribution_matrix()
+            sage: lat = GS.linear_approximation_table()
+            sage: gb_lat = GS.gb_linear_approximation_table()
+            sage: lat == gb_lat
+            True
+        """
+        m, n = self.input_size(), self.output_size()
+        nrows, ncols = 1 << m, 1 << n
+        gblb = self.gb_linear_bias
+        entries = [ gblb(a, b) for a in range(nrows) for b in range(ncols)]
+        return Matrix(ZZ, entries, nrows=nrows, ncols=ncols)
+
+
+    def gb_autocorrelation_table(self):
+        """
+        Return the autocorrelation table using Grobner bases algorithm.
+
+        EXAMPLES::
+
+            sage: from gbsbox import GBSBox
+            sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2)
+            sage: act = GS.autocorrelation_table()
+            sage: gb_act = GS.gb_autocorrelation_table()
+            sage: act == gb_act
+            True
+        """
+        m, n = self.input_size(), self.output_size()
+        nrows, ncols = 1 << m, 1 << n
+        gbac = self.gb_autocorrelation
+        entries = [ gbac(a, b) for a in range(nrows) for b in range(ncols)]
+        return Matrix(ZZ, entries, nrows=nrows, ncols=ncols)
+
+
+    def gb_difference_distribution_table(self):
+        """
+        Return the difference distribution table using Grobner bases algorithm.
+
+        EXAMPLES::
+
+            sage: from gbsbox import GBSBox
+            sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2)
+            sage: ddt = GS.difference_distribution_table()
+            sage: gb_ddt = GS.gb_difference_distribution_table()
             sage: ddt == gb_ddt
             True
         """
@@ -349,7 +563,26 @@ class GBSBox(SBox):
         nrows, ncols = 1 << m, 1 << n
         gbdp = self.gb_differential_probability
         entries = [ gbdp(a, b, get_numerator=True) for a in range(nrows) for b in range(ncols)]
-        return Matrix(ZZ, entries, nrows=ncols, ncols=ncols)
+        return Matrix(ZZ, entries, nrows=nrows, ncols=ncols)
+
+
+    def gb_nonlinearity(self):
+        """
+        Return the nonlinearity of this S-Box using Grobner bases algorithm.
+
+        EXAMPLES::
+
+            sage: from gbsbox import GBSBox
+            sage: GS = GBSBox(0, 1, 3, 6, 7, 4, 5, 2)
+            sage: GS.gb_nonlinearity() == GS.nonlinearity()
+            True
+        """
+        m, n = self.input_size(), self.output_size()
+        gblb = self.gb_linear_bias
+
+        nl = (1 << (m-1)) - max( abs(gblb(a, b)) for a in range(1 << m) for b in range(1, 1 << n) )
+
+        return nl
 
 
     def gb_differential_uniformity(self):
